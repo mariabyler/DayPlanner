5db721b1902fb625b70f64cb07f13cf609209c14
Maria Byler <mariabyler98@gmail.com>
2020-10-03 23:53:46 -0400

    added functional timeblocks

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-$(document).ready(function() {});
+$(document).ready(function () {// essentially tells engine to load 1)html & 2)css first.
+    //display current day & time.
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+    //Assign saveBtn click listener for user input and time stamp??
+    $(".saveBtn").on("click", function () {
+        //get nearby values.
+        console.log(this);
+        var text = $(this).siblings(".text").val();
+        var time = $(this).parent().attr("id");
+        //set items in local storage.
+        localStorage.setItem(time, text);
+    })
+    //load any saved data from LocalStorage - do this for each hour created.
+    $("#hour8 .text").val(localStorage.getItem("hour8"));
+    $("#hour9 .text").val(localStorage.getItem("hour9"));
+    $("#hour10 .text").val(localStorage.getItem("hour10"));
+    $("#hour11 .text").val(localStorage.getItem("hour11"));
+    $("#hour12 .text").val(localStorage.getItem("hour12"));
+    $("#hour13 .text").val(localStorage.getItem("hour13"));
+    $("#hour14 .text").val(localStorage.getItem("hour14"));
+    $("#hour15 .text").val(localStorage.getItem("hour15"));
 
-function getLocalStorage(time, task) { 
-    var a = localStorage.getItem(time); 
-    task.innerHTML = a; 
-}
-
-$("#save1").on("click", function() {
-// retrieve value from text area and put in variable
-var task = $("text1").val(); 
-// create another variable and make it equal to attribute with time
-var time = $("#time1").attr("time"); 
-// put both variables in local storage
-localStorage.setItem(time, task); 
-console.log(task); 
-}
-); 
-
-
-console.log(localStorage); 
-
+}); 
