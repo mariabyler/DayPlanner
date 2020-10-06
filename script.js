4c11ec1cebc1c29aa660f3fcaa438f8d71adfec1
Maria Byler <mariabyler98@gmail.com>
2020-10-06 01:27:27 -0400

    added local storage function

--- script.js
+++ script.js
@@ -1,30 +1,11 @@
-$(document).ready(function () {// essentially tells engine to load 1)html & 2)css first.
-    //display current day & time.
+$( document ).ready(function() {
+    console.log( "ready!" );
     function update() {
         $('#currentDay').html(moment().format('MMMM Do YYYY, h:mm:ss a'));
       }
-    
-     var calender = $.map($("div[time]"), function(div) {
-        return $(div).attr("time");
-    });
-    
-    console.log(calender, "time"); 
-    
-    // calender.forEach(function(){{
-    //     if (calender.moment === "ago" ) {
-    //     calender.style.color = ".past";
-    //     } 
-    //     else if (calender.moment === "from now") {
-    //         calender.style.color = ".future";
-    //         }
-    //         else {
-    //             calender.style.color = ".present";
-    //         }
-    // };
-    });
-    setInterval(update, 1000);
-    
-    //Assign saveBtn click listener for user input and time stamp??
+      setInterval(update, 1000);
+      
+      //on clicks to save time and user input
     $(".saveBtn").on("click", function () {
         //get nearby values.
         console.log(this);
@@ -32,7 +13,7 @@ $(document).ready(function () {// essentially tells engine to load 1)html & 2)cs
         var time = $(this).parent().attr("id");
         //set items in local storage.
         localStorage.setItem(time, text);
-    })
+    }); 
     //load any saved data from LocalStorage - do this for each hour created.
     $("#hour8 .text").val(localStorage.getItem("hour8"));
     $("#hour9 .text").val(localStorage.getItem("hour9"));
@@ -43,4 +24,44 @@ $(document).ready(function () {// essentially tells engine to load 1)html & 2)cs
     $("#hour14 .text").val(localStorage.getItem("hour14"));
     $("#hour15 .text").val(localStorage.getItem("hour15"));
 
-}); 
+    function timetracker() {
+        // array of time attributes
+        var timeAttr = $.map($("div[time]"), function(div) {
+            return $(div).attr("time");
+        });
+        console.log(timeAttr, "time attr"); 
+
+        var timeChunk = parseInt(timeAttr); 
+        for (i = 0; i < timeChunk.length; i++) {}
+        console.log(timeChunk, "var for comparison"); 
+
+        //get current number of hours.
+        var currenttime = moment().hour();
+        console.log(currenttime, "moment js current time"); 
+        // loop over time blocks
+            $(".text").each(function() { 
+                //check if we've moved past this time
+                if (timeChunk[i] < currenttime) {
+                    $(this).addClass("past");
+                    $(this).removeClass("future");
+                    $(this).removeClass("present");
+                }
+                else if (timeChunk[i] === currenttime) {
+                    $(this).removeClass("past");
+                    $(this).addClass("present");
+                    $(this).removeClass("future");
+                }
+                else {
+                    $(this).removeClass("present");
+                    $(this).removeClass("past");
+                    $(this).addClass("future");
+                }
+            }); 
+    }
+timetracker();
+
+});
+
+
+
+
